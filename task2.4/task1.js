1654b0f38b894cd5aaf7a51b8a08a8b74baa6a17
KaterynaVoloshina <katvol16@gmail.com>
2026-03-07 08:46:33 +0200

    first commit

---
+++ task2.4/task1.js
@@ -0,0 +1,16 @@
+'use script'
+// Завдання: робота зі змінними
+let admin, name;
+name = "Іван";
+admin = name;
+alert(admin);
+
+//Завдання: правильні імена для змінних
+let earthName='Земля';
+let currentUser='Катерина';
+alert(earthName);
+alert(currentUser);
+
+// Використання вел та мал літер та цифр
+
+let 
